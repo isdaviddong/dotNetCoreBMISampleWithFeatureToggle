0dfcf0102b1df747c6e030311f96f94306a6603c
twdeveloper <twdeveloper@outlook.com>
2022-09-14 00:09:23 +0800

    v1

---
+++ WebBMI/WebBMI/Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.FeatureManagement;
+
+namespace WebBMI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public float BmiResult = 0;
+        public bool isEnabledFeatureGender = false;
+
+        [BindProperty]
+        public int fieldHeight { get; set; }
+        [BindProperty]
+        public int fieldWeight { get; set; }
+
+        public readonly IConfiguration Configuration;
+        public IndexModel(IConfiguration configuration, IFeatureManager featureManager)
+        {
+            Configuration = configuration;
+
+            if (featureManager != null)
+                isEnabledFeatureGender = featureManager.IsEnabledAsync("featureGender").Result;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPostCalculate()
+        {
+            HealthMgr.BmiCalculator bc = new HealthMgr.BmiCalculator();
+
+            bc.Height = fieldHeight;
+            bc.Weight = fieldWeight;
+
+            BmiResult = bc.Calculate() ;
+        }
+    }
+}
---
+++ WebBMI/WebBMI/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
+namespace WebBMI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+             Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                }).ConfigureAppConfiguration((ctx, builder) =>
+                {
+                    var settings = builder.Build();
+
+                    // This section can be used to pull feature flag configuration from Azure App Configuration
+                    builder.AddAzureAppConfiguration(o =>
+                    {
+                        o.Connect("Endpoint=https://appconfig20220913.azconfig.io;Id=mT5u-lb-s0:IVtc27knZ/y7HAnAwdHM;Secret=jd6CLPeT5ws+K99pbyatT38yt++u6KuJFayvFDYVhas=");
+                        o.Select(KeyFilter.Any);
+                        o.UseFeatureFlags(FeatureFlagconfig =>
+                        {
+                            FeatureFlagconfig.CacheExpirationInterval = TimeSpan.FromSeconds(1);
+                        });
+                    });
+                });
+    }
+}
